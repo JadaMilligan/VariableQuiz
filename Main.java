d41113a760fd305a4d67a21b7e7fae8aa4bf8b16
JadaMilligan <70172897+JadaMilligan@users.noreply.github.com>
2020-09-02 16:11:51 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mallHours = 5;
+        float outfit = 20.55f;
+        boolean feeling = true;
+        String hadFun = "Of course!";
+        System.out.println("I spent "+mallHours+" hours at the mall.");
+        System.out.println("I got an outfit for "+outfit+", it was a steal.");
+        System.out.println("It's "+feeling+", I had a great time!");
+        System.out.println("Would I do this again? "+hadFun);
+    }
+}
